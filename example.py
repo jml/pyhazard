59171e7acabfac355e803ebe47ec8cb7743dd288
Jonathan Lange <jml@mumak.net>
2015-06-28 11:52:39 +0100

    Get up to the point of choosing a play

---
+++ example.py
@@ -0,0 +1,116 @@
+# Copyright (c) 2015 Jonathan M. Lange <jml@mumak.net>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Simple example client
+"""
+
+# TODO: Export from public name.
+from hazard._client import *
+from hazard._client import _make_credentials
+
+
+"""
+The server running Hazard. https://haverer.jml.io/ in production.
+"""
+BASE_URL = 'http://localhost:3000'
+# TODO: These endpoints ought to be in the library, rather than something that
+# users need to know.
+USERS_ENDPOINT = BASE_URL + '/users'
+GAMES_ENDPOINT = BASE_URL + '/games'
+
+
+def get_game_endpoint(game):
+    # TODO: This also should be in the library.
+    return BASE_URL + game['url']
+
+
+def get_round_endpoint(round_url):
+    # TODO: This also should be in the library.
+    return BASE_URL + round_url
+
+
+def player_info(round_info, player_id):
+    for info in round_info['players']:
+        if info['id'] == player_id:
+            return info
+
+
+def get_status(player):
+    if player['active']:
+        if player['protected']:
+            return ' (protected)'
+        else:
+            return ''
+    else:
+        return ' (eliminated)'
+
+
+def print_round_info(round_info):
+    current_player = round_info['currentPlayer']
+    print 'Players:'
+    for player in round_info['players']:
+        status = get_status(player)
+        if player['id'] == current_player:
+            current = '* '
+        else:
+            current = '  '
+        print '{}{}{}: {}'.format(
+            current, player['id'], status, player['discards'])
+    print
+
+
+def choose_play(hand, dealt_card):
+    print 'choosing play: {}, {}'.format(hand, dealt_card)
+    return hand
+
+
+def main():
+    # Register two users, 'foo' and 'bar'.
+    foo = register_user(USERS_ENDPOINT, 'foo')
+    foo_creds = _make_credentials(foo)
+    bar = register_user(USERS_ENDPOINT, 'bar')
+    bar_creds = _make_credentials(bar)
+
+    users = pmap({
+        foo['id']: foo,
+        bar['id']: bar,
+    })
+
+    # 'foo' creates a 2-player game
+    game = register_game(foo_creds, GAMES_ENDPOINT, 2)
+    game_url = get_game_endpoint(game)
+
+    # 'bar' joins the game, and the game begins.
+    join_game(bar_creds, game_url)
+    while True:
+        game = get_game_info(None, game_url)
+        if game['state'] != 'in-progress':
+            break
+        current_round = get_round_endpoint(game['currentRound'])
+        round_info = get_round_info(None, current_round)
+        print_round_info(round_info)
+        current_player_id = round_info['currentPlayer']
+        current_player = users[current_player_id]
+        current_player_view = get_round_info(
+            _make_credentials(current_player), current_round)
+        dealt_card = current_player_view['dealtCard']
+        hand = player_info(current_player_view, current_player_id)['hand']
+        play = choose_play(dealt_card, hand)
+        print play
+        break
+
+
+if __name__ == '__main__':
+    main()
--- hazard/_client.py
+++ hazard/_client.py
@@ -23,9 +23,51 @@ from pyrsistent import pmap
 
 def register_user(users_endpoint, username):
     data = {'username': username}
-    return pmap(requests.post(users_endpoint, data=json.dumps(data)).json())
+    result = pmap(requests.post(users_endpoint, data=json.dumps(data)).json())
+    return result.update(data)
 
 
-def get_user_info(username, password, user_info_endpoint):
+def _make_credentials(registration_data):
+    return (registration_data['username'], registration_data['password'])
+
+
+def get_user_info(credentials, user_info_endpoint):
     return pmap(
-        requests.get(user_info_endpoint, auth=(username, password)).json())
+        requests.get(
+            user_info_endpoint, auth=credentials,
+            headers={'Accept': 'application/json'}
+        ).json())
+
+# TODO: Handle errors
+
+# TODO: Encode routes in functions, rather than requiring full URLs to be
+# passed in.
+
+
+def register_game(credentials, game_endpoint, num_players, turn_timeout=3600):
+    data = {'numPlayers': num_players, 'turnTimeout': turn_timeout}
+    response = requests.post(
+        game_endpoint, data=json.dumps(data), auth=credentials)
+    result = response.json()
+    result['url'] = response.headers['location']
+    return pmap(result)
+
+
+def join_game(credentials, game_endpoint):
+    return pmap(requests.post(game_endpoint, data='', auth=credentials).json())
+
+
+def get_game_info(credentials, game_endpoint):
+    return pmap(
+        requests.get(
+            game_endpoint, auth=credentials,
+            headers={'Accept': 'application/json'},
+        ).json())
+
+
+def get_round_info(credentials, round_endpoint):
+    return pmap(
+        requests.get(
+            round_endpoint, auth=credentials,
+            headers={'Accept': 'application/json'},
+        ).json())
